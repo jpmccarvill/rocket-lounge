edf43e6ff469cfee4da229d8b28442bae9758073
jpmccarvill <jpmccarvill@gmail.com>
2021-06-09 10:15:06 -0400

    Update to Unity 2020

--- Starship/Assets/Plugins/signin.bat
+++
@@ -1,3 +0,0 @@
-echo off
-title Sign In to AltspaceVR
-curl -v -d "user[email]=jpmccarvill@gmail.com&user[password]=BMW330ci!" https://account.altvr.com/users/sign_in.json -c cookie
--- Starship/Assets/Plugins/upload.bat
+++
@@ -1,2 +0,0 @@
-echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\jpmcc\Starship\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1668333186410611137.json
